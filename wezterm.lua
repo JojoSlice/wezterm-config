60be1a91c6b50c4d03830948789631deb9b38135
Jojo Lindell <johannes152@hotmail.com>
2025-03-24 11:30:29 +0100

    keymaps

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,15 +7,15 @@ local config = wezterm.config_builder()
 -- Font
 config.font_size = 14
 config.line_height = 1.2
-config.font = wezterm.font 'Cascadia Code'
+config.font = wezterm.font("Cascadia Code")
 
 -- Color scheme:
-config.color_scheme = 'Shaman (Gogh)'
+config.color_scheme = "Shaman (Gogh)"
 
 -- Appearance:
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_image = 'C:/Users/Johan/Pictures/bakgrunder/wezterm.png'
+config.window_background_image = "C:/Users/Johan/Pictures/bakgrunder/wezterm.png"
 config.window_background_image_hsb = {
 	brightness = 0.3,
 	hue = 1.0,
@@ -23,6 +23,44 @@ config.window_background_image_hsb = {
 }
 --Other:
 config.max_fps = 60
-
+--KeyBinds
+config.keys = {
+	{
+		key = "t",
+		mods = "CTRL",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "Tab",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "Tab",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+}
 
 return config
